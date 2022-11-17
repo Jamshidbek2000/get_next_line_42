083c3e8b92648b52fac00ddc58e946523d2ae158
jergashe <jergashe@student.42heilbronn.de>
2022-11-17 15:44:28 +0100

    ready code

---
+++ get_next_line_bonus.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergashe <jergashe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 16:35:22 by jergashe          #+#    #+#             */
+/*   Updated: 2022/11/17 11:20:44 by jergashe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+// The function reads BUFFER_SIZE length string from file with fd
+// Until the string doesn't have a \n 
+// Or Until there is something to read()
+// And saves what was read to static char * buff.
+// If there is a problem the function frees buff and return NULL
+char	*ft_read(t_file_obj *file, char *buff)
+{
+	char	tmp_buffer[BUFFER_SIZE + 1];
+	int		read_size;
+
+	read_size = 1;
+	while (containts_new_line(buff) == -1 && read_size > 0)
+	{
+		read_size = read(file->fd, tmp_buffer, BUFFER_SIZE);
+		if (read_size == -1)
+			return (free(buff), NULL);
+		tmp_buffer[read_size] = '\0';
+		buff = ft_strjoin(buff, tmp_buffer);
+		if (buff == NULL || ft_strlen(buff) == 0)
+			return (free(buff), NULL);
+	}
+	return (buff);
+}
+
+// The function returns the part of buff until \n or \0
+char	*ft_get_line(t_file_obj *file, char *buff)
+{
+	char	*tmp;
+
+	if (buff == NULL || ft_strlen(buff) == 0)
+		return (NULL);
+	file->new_line_index = containts_new_line(buff);
+	if (file->new_line_index >= 0)
+		return (ft_strndup(buff, 0, file->new_line_index + 1));
+	tmp = ft_strndup(buff, 0, ft_strlen(buff));
+	return (tmp);
+}
+
+// The function returns part after \n if it exists
+char	*ft_get_rest(t_file_obj *file, char *buff)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	if (buff == NULL || ft_strlen(buff) == 0)
+		return (free(buff), NULL);
+	if (file->new_line_index != -1)
+		tmp = ft_strndup(buff, file->new_line_index + 1, ft_strlen(buff));
+	free(buff);
+	return (tmp);
+}
+
+// The function gets part of file until \n or \0
+// under the file descriptor fd
+char	*get_next_line(int fd)
+{
+	static char	*buff[OPEN_MAX];
+	t_file_obj	*file;
+	char		*result;
+
+	if (read(fd, NULL, 0) == -1)
+		return (free(buff[fd]), buff[fd] = NULL, NULL);
+	file = get_t_file_obj(fd);
+	buff[fd] = ft_read(file, buff[fd]);
+	result = ft_get_line(file, buff[fd]);
+	buff[fd] = ft_get_rest(file, buff[fd]);
+	free(file);
+	return (result);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jergashe <jergashe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 13:51:52 by jergashe          #+#    #+#             */
+/*   Updated: 2022/11/17 14:51:49 by jergashe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <limits.h>
+# include <stdlib.h>
+
+typedef struct t_file_obj
+{
+	int		new_line_index;
+	int		fd;
+}t_file_obj;
+
+char		*get_next_line(int fd);
+
+int			containts_new_line(char *str);;
+t_file_obj	*get_t_file_obj(int new_fd);
+int			ft_strlen(const char *s);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strndup(char *str, int start, int end);
+
+#endif
